3f99a17547e59d16f9f858da0418723dc6cbc2d7
prashantgautam96 <2020ucs0114@iitjammu.ac.in>
2023-03-27 03:30:03 +0530

    Feature: Add Prisma

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,46 +1,53 @@
+//* node-graphql/src/resolvers.js
 
-const { students } =  require('./database.js');
+const { prisma } = require("./database.js");
 
-const resolvers = {
+const Student = {
+  id: (parent, args, context, info) => parent.id,
+  email: (parent) => parent.email,
+  fullName: (parent) => parent.fullName,
+  dept: (parent) => parent.dept,
+  enrolled: (parent) => parent.enrolled,
+};
 
-    Student: {
-        id: (parent, args, context, info) => parent.id,
-        email: (parent) => parent.email,
-        fullName: (parent) => parent.fullName,
-        dept: (parent) => parent.dept,
-        enrolled: (parent) => parent.enrolled,
+const Query = {
+  enrollment: (parent, args) => {
+    return prisma.student.findMany({
+      where: { enrolled: true },
+    });
+  },
+  students: (parent, args) => {
+    return prisma.student.findMany({});
+  },
+  student: (parent, args) => {
+    return prisma.student.findFirst({
+      where: { id: Number(args.id) },
+    });
+  },
+};
+
+const Mutation = {
+  registerStudent: (parent, args) => {
+    return prisma.student.create({
+      data: {
+        email: args.email,
+        fullName: args.fullName,
+        dept: args.dept,
       },
-
-    Query: {
-      enrollment: (parent, args) => {
-        return students.filter((student) => student.enrolled)
+    });
+  },
+  enroll: (parent, args) => {
+    return prisma.student.update({
+      where: { id: Number(args.id) },
+      data: {
+        enrolled: true,
       },
-      student: (parent, args) => {
-        return students.find((student) => student.id === Number(args.id))
-      },
-    },
+    });
+  },
+};
 
-    Mutation: {
-      registerStudent: (parent, args) => {
-        students.push({
-          id: students.length + 1,
-          email: args.email,
-          fullName: args.fullName,
-          dept: args.dept,
-          enrolled: false,
-        })
-        return students[students.length - 1]
-      },
-      enroll: (parent, args) => {
-        const studentToEnroll = students.find((student) => student.id === Number(args.id))
-        studentToEnroll.enrolled = true
-        return studentToEnroll
-      },
-    },
+const resolvers = { Student, Query, Mutation };
 
-  }
-
-
-  module.exports = {
-    resolvers,
-  }
+module.exports = {
+  resolvers,
+};
